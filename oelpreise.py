9611cdc509d6992f9d56e9df38954f778a55a380
PhilKret <117720315+PhilKret@users.noreply.github.com>
2022-11-09 11:37:13 +0100

    oelpreisabfrage  script

---
+++ oelpreise.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import requests
+
+
+# In[7]:
+
+
+# xls files mit pd.read_excel() auslesen
+# Excel Tabelle besteht aus mehrere Sheets --> anwählen über sheet_name=
+# mit skiprows= überspringen wir Zeilen, die wir nicht brauchen
+
+df = pd.read_excel('https://www.eia.gov/dnav/pet/hist_xls/RBRTEd.xls', sheet_name='Data 1', skiprows=2)
+
+
+# In[6]:
+
+
+df
+
+
+# In[8]:
+
+
+# Spalten anders benennen
+
+df.columns = ['Datum', 'Dollars pro Barrel']
+
+
+# In[9]:
+
+
+df
+
+
+# In[10]:
+
+
+df.info() # erkennt er das Datumsformat?
+
+
+# In[15]:
+
+
+df = df.set_index('Datum') # Datum als Index
+
+
+# In[17]:
+
+
+df_resampled = df.resample('M').mean() # monatlichen Durchschnitt berechnen mit resample().mean()
+
+
+# In[18]:
+
+
+df_resampled # unser dataframe ist fertig
+
+
+# In[19]:
+
+
+df_resampled.plot.line()
+
+
+# In[20]:
+
+
+df_resampled.to_csv('oelpreis.csv') #csv export
+
+
+# In[ ]:
+
+
+
+
